f85cbf98398af65f33019648e09e305b02994dda
Ian Jones <eab2-jackson@STU-CZC42008CX.campus.ads.uwe.ac.uk>
2016-01-30 15:04:43 +0000

    Added scene and basic Villager managment

---
+++ SupFlusGGJ16/Assets/Scripts/Villager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+/// <summary>
+/// Class for controller Villager
+/// </summary>
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class Villager : MonoBehaviour
+{
+    private PlatformerCharacter2D m_Character;
+    private bool m_Jump;
+    [SerializeField] float xDir;
+
+    private float health = 1;
+
+    //Target X position for Villager to aim for when they're waiting in queue
+    public float targetX;
+
+    /// <summary>
+    /// Whether the Villager is advancing in the queue
+    /// </summary>
+    public bool advancing;
+
+    /// <summary>
+    /// If the Villager alive?
+    /// </summary>
+    public bool alive
+    {
+        get
+        {
+            return health > 0;
+        }
+    }
+
+    //Whether it's currently in control by the player
+    public bool activePlayer = false;
+
+    private void Awake()
+    {
+        m_Character = GetComponent<PlatformerCharacter2D>();
+    }
+
+    private void Update()
+    {
+        if (alive)
+        {
+            if (activePlayer)
+            {   
+
+                xDir = 0;
+                xDir = ((Input.GetKey(KeyCode.D)) ? 1 : xDir);
+                xDir = ((Input.GetKey(KeyCode.A)) ? -1 : xDir);
+
+                if (!m_Jump)
+                {
+                    // Read the jump input in Update so button presses aren't missed.
+                    m_Jump = Input.GetKeyDown(KeyCode.Space);
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(transform.localPosition.x - targetX) > .5f)
+                {
+                    xDir = Mathf.Clamp01(targetX - transform.localPosition.x);
+                }
+                else
+                {
+                    advancing = false;
+                }   
+            }
+        }
+        else
+        {
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (activePlayer)
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+
+            xDir = ((Input.GetKey(KeyCode.D)) ? 1 : xDir);
+            xDir = ((Input.GetKey(KeyCode.A)) ? -1 : xDir);
+
+            // Pass all parameters to the character control script.
+            m_Character.Move(xDir, crouch, m_Jump);
+            m_Jump = false;
+        }
+        else
+        {
+            m_Character.Move(xDir, false, false);
+        }
+    }
+    
+    /// <summary>
+    /// Kills player
+    /// </summary>
+    public void Kill()
+    {
+        health = 0;
+    }
+
+    /// <summary>
+    /// Sets target for Villager to aim for in the x axis
+    /// </summary>
+    /// <param name="xPos">Target X Position</param>
+    public void SetTarget(float xPos)
+    {
+        targetX = xPos;
+        advancing = true;
+    }
+}
+
---
+++ SupFlusGGJ16/Assets/Scripts/VillagerManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VillagerManager : MonoBehaviour {
+
+    public Transform    activeVillagerTrans,
+                        remainingVillagersTrans,
+                        warpGateEnt,
+                        warpGateExit;
+
+    Villager activeVillager;
+
+    [SerializeField] List<Villager> remainingVillagers;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        remainingVillagers = new List<Villager>();
+
+        Villager[] villagers = remainingVillagersTrans.GetComponentsInChildren<Villager>();
+
+        remainingVillagers.AddRange(villagers);
+
+	    if(activeVillager == null)
+        {
+            Villager player = activeVillagerTrans.GetComponentInChildren<Villager>();
+
+            if (player == null)
+            {
+                NextVillager();
+            }
+            else
+            {
+                activeVillager = player;
+            }
+
+            activeVillager.activePlayer = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+#if UNITY_EDITOR
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            activeVillager.Kill();
+        }
+
+#endif
+
+        if (activeVillager.alive)
+        {
+
+        }
+        else
+        {
+            //What needs to happen when active Villager dies
+            //1. Level resets to start
+            //2. previous Villagers begin their tasks
+            //3. Take control of next Villager in list
+            NextVillager();
+            //4. Teleport Villager to middle
+            EnterArena();
+        }
+
+        for(int i = 0; i < remainingVillagers.Count; i++)
+        {
+            //If Villager is not moving forward and not in his correct place
+            if(!remainingVillagers[i].advancing &&
+                remainingVillagers[i].transform.localPosition.x < i * -2)
+            {
+                Debug.Log("Villager " + i + " is not in his correct place");
+                remainingVillagers[i].SetTarget(i * - 2);
+            }
+        }
+	}
+
+    /// <summary>
+    /// Gives player control of next villager
+    /// </summary>
+    void NextVillager()
+    {
+        //Prevent 
+        if (activeVillager)
+            activeVillager.activePlayer = false;
+
+        activeVillager = remainingVillagers[0];
+        remainingVillagers.RemoveAt(0);
+
+        activeVillager.activePlayer = true;
+    }
+
+    void EnterArena()
+    {
+        activeVillager.transform.position = warpGateExit.transform.position; 
+    }
+
+}
