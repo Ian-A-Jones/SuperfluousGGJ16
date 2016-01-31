dfb6e081a01433998ab32bea3640560d9233c662
Ian Jones <enigma994@live.co.uk>
2016-01-31 06:51:24 +0000

    Boss loops

--- SupFlusGGJ16/Assets/Scripts/Boss.cs
+++ SupFlusGGJ16/Assets/Scripts/Boss.cs
@@ -5,43 +5,93 @@ public class Boss : MonoBehaviour {
 
     public Animator leftArm, rightArm;
 
-    Timer LAttack1, LAttack2;
+    Timer LAttack1, RAttack1,
+          LAttack2, RAttack2;
+
+    int totalSweeps = 3, currentSweeps = 0,
+        totalSlams = 4, currentSlams = 0;
 	// Use this for initialization
 	void Start () {
 
         LAttack1 = gameObject.AddComponent<Timer>();
-        LAttack1.Setup("LeftHandAttack", 1, true);
+        LAttack1.Setup("LeftHandAttack", 2, true);
 
         LAttack1.StartTimer();
 
+        RAttack1 = gameObject.AddComponent<Timer>();
+        RAttack1.Setup("RightHandAttack", 2.5f, true);
+        RAttack1.enabled = false;
+
         LAttack2 = gameObject.AddComponent<Timer>();
-        LAttack2.Setup("RightHandAttack", 1, true);
-        LAttack2.enabled = false;
-        //LAttack2.StartTimer();
+        LAttack2.Setup("LHandSlam", 1f, true);
+
+        RAttack2 = gameObject.AddComponent<Timer>();
+        RAttack2.Setup("RHandSlam", 1f, true);
+
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
-        if(LAttack1.complete)
+        if (currentSweeps <= totalSweeps)
         {
-            LAttack1.Reset();
-            leftArm.SetBool("Attack1", true);
+            if (LAttack1.complete)
+            {
+                currentSweeps++;
+                LAttack1.Reset();
+                leftArm.SetBool("Attack1", true);
 
-            LAttack1.enabled = false;
+                LAttack1.enabled = false;
 
-            LAttack2.enabled = true;
-            LAttack2.StartTimer();
+                RAttack1.enabled = true;
+                RAttack1.StartTimer();
+            }
+
+            if (RAttack1.complete)
+            {
+                currentSweeps++;
+                rightArm.SetBool("Attack1", true);
+                RAttack1.Reset();
+                RAttack1.enabled = false;
+
+                LAttack1.enabled = true;
+                LAttack1.StartTimer();
+            }
         }
-
-        if (LAttack2.complete)
+        else if(currentSlams <= totalSlams)
         {
-            rightArm.SetBool("Attack1", true);
-            LAttack2.Reset();
-            LAttack2.enabled = false;
+            if(currentSlams == 0 && !LAttack2.active && !LAttack2.complete)
+            {
+                LAttack2.StartTimer();
+            }
+            if (LAttack2.complete)
+            {
+                currentSlams++;
+                LAttack2.Reset();
+                leftArm.SetBool("Attack2", true);
 
-            LAttack1.enabled = true;
-            LAttack1.StartTimer();
+                LAttack2.enabled = false;
+
+                RAttack2.enabled = true;
+                RAttack2.StartTimer();
+            }
+
+            if (RAttack2.complete)
+            {
+                currentSlams++;
+                rightArm.SetBool("Attack2", true);
+                RAttack2.Reset();
+                RAttack2.enabled = false;
+
+                LAttack2.enabled = true;
+                LAttack2.StartTimer();
+            }
+        }
+        else
+        {
+            currentSweeps = 0;
+            currentSlams = 0;
         }
 
     }
